6030866e50a46a43efd36ac2038dd5d54fd9b671
sanjana-ayshi <sanjana.ayshi92@gamil.com>
2022-06-16 18:51:01 +0600

    secound  commit

---
+++ truthly-falsly.js
@@ -0,0 +1,16 @@
+//falsely
+//false
+//undefined
+//0
+//""
+//null
+//NaN
+//truthy
+//'0'," ", [];
+let name=[];
+if(name){
+    console.log("it's true");
+}
+else{
+    console.log("it's false");
+}
